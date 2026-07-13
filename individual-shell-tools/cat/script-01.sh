79e87742240a7334d230d69df21d28e6014e04ab
mervereis <mervereis78@gmail.com>
2026-07-13 22:45:13 +0100

    Fix command syntax in various scripts by removing comment slashes

--- individual-shell-tools/cat/script-01.sh
+++ individual-shell-tools/cat/script-01.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 # TODO: Write a command to output the contents of the helper-1.txt file inside the helper-files directory to the terminal.
 # The output of this command should be "Once upon a time...".
 
-//mkdir helper-files
-//cd helper-files
-//cat > helper-1.txt
-//Once upon a time...
-//press the return key and then control+D
+mkdir helper-files
+cd helper-files
+cat > helper-1.txt
+Once upon a time...
+press the return key and then control+D
